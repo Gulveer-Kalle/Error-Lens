90422b8eb1a6a0aaf7f2df6edad7a63d4e55a6ae
gulveer <gulveerkalle@gmail.com>
2026-06-12 14:13:25 -0400

    Fix frontend SPA fallback middleware for Express route handling

--- frontend/server.js
+++ frontend/server.js
@@ -9,7 +9,7 @@ const distPath = path.join(__dirname, 'dist', 'frontend', 'browser');
 app.use(express.static(distPath));
 
 // SPA fallback - redirect all routes to index.html
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(distPath, 'index.html'));
 });
 
